4c3226be460c03184704c495cf973352ec329a76
lehuuchinh <155989379+lehuuchinh@users.noreply.github.com>
2024-09-05 23:51:26 +0700

    Add files via upload

---
+++ class.js
@@ -0,0 +1,13 @@
+class Student {
+    constructor(id, name, gender, date, address) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.date = date;
+        this.address = address;
+    }
+
+    let displayInformations = () => {
+        
+    }
+}
